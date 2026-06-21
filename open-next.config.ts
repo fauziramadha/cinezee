efc014a613c21461c41c36540e7f9ddf346dfc22
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-06-21 22:30:07 +0700

    Create open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import memoryQueue from "@opennextjs/cloudflare/overrides/queue/memory-queue";
+
+export default defineCloudflareConfig({
+  enableCacheInterception: true,
+  queue: memoryQueue,
+});
