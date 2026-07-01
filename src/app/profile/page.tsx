da929bf44c4d414a0b60c4e296631497b7d3017c
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-07-01 23:38:59 +0700

    Disable SSG for profile page
    Disable SSG for the profile page by setting dynamic to 'force-dynamic'.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic"; // Nonaktifkan SSG untuk halaman ini
+
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
