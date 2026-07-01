57ede84c7311438a8d3423673f20cf9bdf2a5f07
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-07-02 01:59:22 +0700

    Update client-layout.tsx

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,15 +1,6 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-// SessionProvider dipakai dynamic ssr:false agar hook useSession tidak dieval saat build SSG
-const SessionProviderWrapper = dynamic(
-  () => import("@/components/providers/session-provider").then(mod => mod.SessionProviderWrapper),
-  { 
-    ssr: false,
-    loading: () => null 
-  }
-);
+import { SessionProviderWrapper } from "@/components/providers/session-provider";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return <SessionProviderWrapper>{children}</SessionProviderWrapper>;
