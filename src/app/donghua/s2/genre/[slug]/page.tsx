9cee2a2df8127bd04c2d94977751caf743112773
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-07-06 13:02:14 +0700

    Create page.tsx

---
+++ src/app/donghua/s2/genre/[slug]/page.tsx
@@ -0,0 +1,5 @@
+import { DonghuaGenreContent } from "@/components/donghua/donghua-genre-content";
+export default async function S2GenrePage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  return <DonghuaGenreContent slug={slug} source="s2" />;
+}
