318e9b24c1b4aaf3a333a8a1cbf521c696474f5c
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-07-07 14:06:24 +0700

    Update page.tsx

--- src/app/donghua/page.tsx
+++ src/app/donghua/page.tsx
@@ -1,2 +1,7 @@
-import { DonghuaContent } from "./donghua-content";
-export default function DonghuaPage() { return <DonghuaContent />; }
+import { DonghuaS1Content } from "./s1/content";
+
+export const dynamic = "force-dynamic";
+
+export default function DonghuaPage() {
+  return <DonghuaS1Content />;
+}
