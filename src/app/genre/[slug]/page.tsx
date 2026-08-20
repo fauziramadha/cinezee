8f2bdce7da07aef78ac0a632ec3aca926cae3e2f
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-08-20 20:19:37 +0700

    Update page.tsx

--- src/app/genre/[slug]/page.tsx
+++ src/app/genre/[slug]/page.tsx
@@ -14,7 +14,7 @@ import { PlayerModal } from "@/components/cinepro/player-modal";
 import { AuthModal } from "@/components/cinepro/auth-modal";
 
 const VPS_API_BASE =
-  process.env.NEXT_PUBLIC_API_URL || "https://api.cinestream.my.id";
+  process.env.NEXT_PUBLIC_API_URL || "https://api.cinestream.biz.id";
 
 export default function GenrePage() {
   const params = useParams();
