dd8e531316be6c2d133f3aa4970277f4b45b7fd4
fauziramadhani <93056712+fauziramadha@users.noreply.github.com>
2026-08-20 20:18:28 +0700

    Update content.tsx

--- src/app/tv/content.tsx
+++ src/app/tv/content.tsx
@@ -13,7 +13,7 @@ import { Button } from "@/components/ui/button";
 import { useAppStore } from "@/lib/store";
 
 const VPS_API_BASE =
-  process.env.NEXT_PUBLIC_API_URL || "https://api.cinestream.my.id";
+  process.env.NEXT_PUBLIC_API_URL || "https://api.cinestream.biz.id";
 
 export function TVContent() {
   const setDetailMedia = useAppStore((s) => s.setDetailMedia);
