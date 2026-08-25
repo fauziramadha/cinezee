6b0aab8c1076213e839a948a374f1abdc5d1e706
Z User <z@container>
2026-08-25 12:06:59 +0000

    Deploy: rebuild .open-next artifacts with VPS FastAPI donghua migration

--- .open-next/server-functions/default/.next/server/chunks/ce889_server_app_api_donghua_donghua_episode_[slug]_route_actions_3646981f.js
+++
@@ -1,3 +0,0 @@
-module.exports=[16001,(e,o,d)=>{}];
-
-//# sourceMappingURL=ce889_server_app_api_donghua_donghua_episode_%5Bslug%5D_route_actions_3646981f.js.map
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GuHHuYOywrtUavkQGlUNazXB00xAKwTz9e2gGsGJYIo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9sCgRHqi57QbesWOuXf6INMeXOmJXu572iNPd7fyWdg=\"\n}"
